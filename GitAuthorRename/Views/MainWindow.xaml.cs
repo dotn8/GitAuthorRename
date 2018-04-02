037456693a90adcff90bd652130ee2e215305eb2
dotn8 <dotn8@users.noreply.github.com>
2018-04-02 16:24:12 -0500

    Create MVVM structure

--- GitAuthorRename/Views/MainWindow.xaml.cs
+++ GitAuthorRename/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GitAuthorRename
+namespace GitAuthorRename.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
